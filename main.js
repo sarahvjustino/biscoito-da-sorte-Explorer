b67cc0435cb2181148a7207b4366bf132051c0fa
Sarah <sarahvictoria230@gmail.com>
2022-12-28 16:51:15 -0300

    Primeiro Commit

---
+++ main.js
@@ -0,0 +1,54 @@
+let messages = [
+    'Se alguém está tão cansado que não possa te dar um sorriso,deixa-lhe o teu',
+    'A vida trará coisas boas se tiver paciência.',
+    'Demonstre amor e alegria em todas as oportunidades e verá que a paz nasce dentro de si.',
+    'Não compense na ira o que lhe falta na razão.',
+    ' Defeitos e virtudes são apenas dois lados da mesma moeda.',
+    'A maior de todas as torres começa no solo.',
+    'Não há que ser forte. Há que ser flexível.',
+    'Todos os dias organiza os seus cabelos, por que não faz o mesmo com o coração?',
+    'Há três coisas que jamais voltam; a flecha lançada, a palavra dita e a oportunidade perdida.',
+    'A juventude não é uma época da vida, é um estado de espírito.',
+    'Podemos escolher o que semear, mas somos obrigados a colher o que plantamos.',
+    'Dê toda a atenção á formação dos seus filhos, sobretudo com bons exemplos da sua própria vida.',
+    'Siga os bons e aprenda com eles.',
+    'Não importa o tamanho da montanha, ela não pode tapar o sol.',
+    'O bom-senso vale mais do que muito conhecimento.',
+    'Quem quer colher rosas tem de estar preparado para suportar os espinhos.',
+    'São os nossos amigos que nos ensinam as mais valiosas lições.',
+    'Aquele que se importa com o sentimento dos outros, não é um tolo.',
+    'A adversidade é um espelho que reflete o verdadeiro eu.',
+    'Lamentar aquilo que não temos é desperdiçar aquilo que já possuímos.',
+]
+let messagePicked = Math.round(Math.random() * messages.length);
+let cookie = document.getElementById('cookie');
+const screen1 = document.querySelector('.screen1');
+const screen2 = document.querySelector('.screen2');
+const btnReset = document.getElementById('btnReset');
+
+document.addEventListener('keypress', enterPressed)
+cookie.addEventListener('click', handleClick);
+btnReset.addEventListener('click', handleResetClick)
+
+function enterPressed(e) {
+    if (e.key == 'Enter' && screen2.classList.contains('hide')) {
+        handleClick()
+    } else if (e.key == 'Enter' && screen1.classList.contains('hide')) {
+        handleResetClick()
+    }
+};
+
+function handleClick() {
+    toggleScreen()
+    screen2.querySelector('p').innerText = messages[messagePicked];
+}
+
+function handleResetClick() {
+    messagePicked = Math.round(Math.random() * messages.length);
+    toggleScreen()
+}
+
+function toggleScreen() {
+    screen1.classList.toggle('hide');
+    screen2.classList.toggle('hide');
+}
